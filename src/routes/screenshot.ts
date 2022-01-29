6b53d457db71fbfabe447745a51958ee3b04bb97
mattcroat <mattcroat.dev@gmail.com>
2022-01-29 23:04:15 +0100

    feat: Add project

---
+++ src/routes/screenshot.ts
@@ -0,0 +1,14 @@
+import * as puppeteer from 'puppeteer'
+
+export async function get() {
+	const browser = await puppeteer.launch()
+	const page = await browser.newPage()
+	await page.setViewport({ width: 1280, height: 720 })
+	await page.goto('http://localhost:3000/')
+	await page.screenshot({ path: `thumbnails/${Date.now()}.png` })
+	await browser.close()
+
+	return {
+		status: 200
+	}
+}
