47ff5d603eb1dd5ce17456b60ba3cd9af5bd97e8
mattcroat <mattcroat.dev@gmail.com>
2022-01-30 21:48:47 +0100

    feat: Add thumbnail

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,68 +1,10 @@
-<script lang="ts">
-	import '$root/styles/global.css'
-
-	async function takeScreenshot() {
-		await fetch('http://localhost:3000/screenshot')
-	}
-</script>
-
-<div class="thumbnail">
-	<span class="order">1</span>
-	<div class="title">
-		<span class="text">what is</span>
-		<span class="text">svelte?</span>
-	</div>
-</div>
-
-<button on:click={takeScreenshot} class="screenshot">Screenshot 📷️</button>
+<h1 class="title">Thumbnail Creator 📸</h1>
 
 <style>
-	.thumbnail {
-		width: 1280px;
-		height: 720px;
-		background-image: url('svelte-for-beginners.png');
-
-		display: grid;
-		grid-template-columns: repeat(12, 1fr);
-		grid-template-rows: repeat(12, 1fr);
-		align-items: center;
-	}
-
-	.order {
-		width: 68px;
-		height: 68px;
-		margin: 2rem;
-		font-family: sans-serif;
-		font-size: 2.4rem;
-		font-weight: bold;
-		color: hsl(15 100% 49%);
-		background-color: hsl(0 0% 100%);
-		border-radius: 50%;
-
-		/* children */
+	.title {
+		height: 100vh;
 		display: grid;
 		place-content: center;
-	}
-
-	.title {
-		grid-column: 6 / -1;
-		grid-row: 1 / -1;
-		font-family: 'Arsenica Trial Bold';
-		font-size: 8.2rem;
-		text-transform: capitalize;
-		color: hsl(0 0% 100%);
-	}
-
-	.text {
-		display: inline-block;
-		margin: 0.6rem 0;
-		padding: 0 1.2rem;
-		background-color: hsl(0 0% 6%);
-		border-radius: 4px;
-	}
-
-	.screenshot {
-		padding: 1rem;
-		font-size: inherit;
+		color: hsl(0, 0%, 100%);
 	}
 </style>
