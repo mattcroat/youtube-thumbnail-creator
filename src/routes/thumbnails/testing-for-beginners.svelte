a0f4e74164abe25802a919cc1f31e87b399fccd6
mattcroat <mattcroat.dev@gmail.com>
2022-01-31 23:05:36 +0100

    feat: Add thumbnail

---
+++ src/routes/thumbnails/testing-for-beginners.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+	async function takeScreenshot() {
+		await fetch('http://localhost:3000/screenshot')
+	}
+</script>
+
+<div class="thumbnail">
+	<span class="order">1</span>
+	<div class="title">
+		<span class="text">Template</span>
+	</div>
+</div>
+
+<button on:click={takeScreenshot} class="screenshot">Screenshot 📷️</button>
+
+<style>
+	.thumbnail {
+		width: 1280px;
+		height: 720px;
+		background-image: url('../testing-for-beginners.png');
+
+		display: grid;
+		grid-template-columns: repeat(12, 1fr);
+		grid-template-rows: repeat(12, 1fr);
+		align-items: center;
+	}
+
+	.order {
+		width: 68px;
+		height: 68px;
+		grid-column: 1 / 2;
+		grid-row: 1 / 3;
+		align-self: end;
+		justify-self: end;
+		margin-top: 3.6rem;
+		font-family: sans-serif;
+		font-size: 2.4rem;
+		font-weight: bold;
+		color: hsl(15 100% 49%);
+		background-color: hsl(0 0% 100%);
+		border-radius: 50%;
+
+		/* children */
+		display: grid;
+		place-content: center;
+	}
+
+	.title {
+		grid-column: 1 / 8;
+		grid-row: 2 / -1;
+		margin-left: 2.4rem;
+		font-family: 'Arsenica Trial Bold';
+		font-size: 7.2rem;
+		text-transform: capitalize;
+		color: hsl(0 0% 100%);
+	}
+
+	.text {
+		display: inline-block;
+		margin: 0.6rem 0;
+		padding: 0 1.2rem;
+		background-color: hsl(0 0% 6%);
+		border-radius: 4px;
+	}
+
+	.screenshot {
+		padding: 1rem;
+		font-size: inherit;
+	}
+</style>
